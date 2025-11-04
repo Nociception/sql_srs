2ff37975bc81ae3221796e70f783a51bac36b4fb
Nociception <nicolas.stoutzer@hotmail.fr>
2025-11-04 17:02:32 +0100

    acc ci

--- app.py
+++ app.py
@@ -8,48 +8,64 @@ def main():
 
     st.write("Spaced Repetition System SQL Practice")
 
-
-    option = st.selectbox(
-        "What would you like to review?",
-        ("Joins", "Group by", "Windows functions"),
-        index=None,
-        placeholder="Select a theme",
-    )
-
-    st.write("You selected:", option)
+    beverages = pl.read_csv("beverages.csv")
+    food_items = pl.read_csv("food_items.csv")
+    QUERY_ANSWER = """
+    SELECT * FROM beverages
+    CROSS JOIN food_items
+    """
+    solution_df = pl.DataFrame(duckdb.sql(QUERY_ANSWER))
 
 
+    with st.sidebar:
+        option = st.selectbox(
+            "What would you like to review?",
+            ("Joins", "Group by", "Windows functions"),
+            index=None,
+            placeholder="Select a theme",
+        )
 
-    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+        st.write("You selected:", option)
 
-    data = {
-        "a": [1, 2, 3],
-        "b": [4, 5, 6],
-    }
-    df = pl.DataFrame(data)
 
-    with tab1:
-        pass
-        query = st.text_area(label="type your query here")
-        st.write(f"You typed: {query}")
-        result = duckdb.query(query).df()
+    query = st.text_area(label="Type your query here")
+    if query:
+        result = pl.DataFrame(duckdb.query(query))
         st.dataframe(result)
 
+        try :
+            result = result.select(solution_df.columns)
+            assert result.equals(solution_df)
+            
+        except pl.exceptions.ColumnNotFoundError:
+            st.write("Some columns are missing")
+        except AssertionError:
+            st.markdown(
+                "<span style='color:red; font-weight:bold'>Error: some values are not the same!</span>",
+                unsafe_allow_html=True
+            )
+            st.dataframe(result)
+            st.write("Expected")
+            st.dataframe(solution_df)
+
+        if delta := abs(result.shape[0] - solution_df.shape[0]) != 0:
+            st.write(f"{delta} lines are missing.")
+
+
+    tab1, tab2 = st.tabs(["Table", "Solution",])
+    with tab1:
+        st.write("beverages")
+        st.dataframe(beverages)
+
+        st.write("food_items")
+        st.dataframe(food_items)
+
+        st.write("Expected result:")
+        st.dataframe(solution_df)
     with tab2:
-        st.header("Dog")
-        st.image(
-            "https://static.streamlit.io/examples/dog.jpg",
-            caption="A friendly dog",
-            width=300
-        )
-        
-    with tab3:
-        st.header("Owl")
-        st.image(
-            "https://static.streamlit.io/examples/owl.jpg",
-            caption="A wise owl",
-            width=300
-        )
+        st.write(QUERY_ANSWER)
+
+
 
 if __name__ == "__main__":
     main()
